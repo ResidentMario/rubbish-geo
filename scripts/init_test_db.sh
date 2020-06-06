e26126ae4ac08072939ea720802ebad13d9b1b91
Aleksey Bilogur <aleksey@residentmar.io>
2020-06-05 20:56:13 -0400

    Fix init script.

--- scripts/init_test_db.sh
+++ scripts/init_test_db.sh
@@ -13,20 +13,37 @@ set -e
 # migration. And we can't init into a conda environment either because running a script in a shell
 # is non-interactive. So for now you just have to run the alembic upgrade head line manually.
 
-# initialize a clean copy of the database
-pg_ctl -D /usr/local/var/postgres stop &>/dev/null || true
-pg_ctl -D /usr/local/var/postgres start || (echo "Could not start Postgres, are you sure it's installed?" && exit 1)
-CONNSTR=postgresql://$(whoami)@localhost/postgres
-psql $CONNSTR -c "DROP DATABASE rubbish;" &>/dev/null
-psql $CONNSTR -c "CREATE DATABASE rubbish;" &>/dev/null
+# initialize Postgres
+# PGDATA envvar is required by pg_ctl status
+RUBBISH_PGSTATUS=$(pg_ctl status -D /usr/local/var/postgres) || true
+echo $RUBBISH_PGSTATUS
+if [[ $RUBBISH_PGSTATUS == *"PID"* ]]; then
+    echo "Postgres is already running, skipping initialization..."
+else
+    pg_ctl -D /usr/local/var/postgres start ||
+    (echo "Could not start Postgres, are you sure it's installed?" && exit 1)
+fi
+echo "FOO"
 
-# run the database migrations
-psql $CONNSTR -c "CREATE EXTENSION postgis;" &>/dev/null || true
+# initialize Rubbish database
+CONNSTR=postgresql://$(whoami)@localhost/postgres
+psql $CONNSTR -c "DROP DATABASE IF EXISTS rubbish;"
+psql $CONNSTR -c "CREATE DATABASE rubbish;"
+CONNSTR=postgresql://$(whoami)@localhost/rubbish
+
+# enable postgis and set up migration script
+POSTGIS_ENABLED=$(psql -qtAX $CONNSTR -c "SELECT extname FROM pg_extension WHERE extname LIKE 'postgis';")
+if [[ $POSTGIS_ENABLED != "postgis" ]]; then
+    psql $CONNSTR -c "CREATE EXTENSION postgis;"
+fi
+
 pushd ../ && RUBBISH=$(PWD) && popd
-# NOTE: uses "|"" as delimiter character because CONNSTR includes "/" characters,
-# Cf. https://backreference.org/2010/02/20/using-different-delimiters-in-sed/
+# NOTE(aleksey): uses "|"" as delimiter character because CONNSTR includes "/" characters
 cat $RUBBISH/python/rubbish/common/alembic.ini |
     sed -E "s|sqlalchemy.url = [a-zA-Z:/_0-9@\.-]*|sqlalchemy.url = $CONNSTR|" > $RUBBISH/python/rubbish/common/test_alembic.ini
-cd $RUBBISH/python/rubbish/common && alembic -c test_alembic.ini upgrade head ||
-    echo "Failed to run migration, are you sure Alembic is installed?"
 cd $RUBBISH/scripts/
+
+# NOTE(aleksey): cannot run alembic op from here due to weird interactions with conda.
+# cd $RUBBISH/python/rubbish/common && alembic -c test_alembic.ini upgrade head ||
+#     echo "Failed to run migration, are you sure Alembic is installed?"
+echo "Almost done! To finish setup navigate to common and run alembic -c test_alembic.ini upgrade head."
