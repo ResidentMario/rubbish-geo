7bd511497b9e2b7161fa6cd05e3ea9a5ee81f45e
Aleksey Bilogur <aleksey@residentmar.io>
2020-07-08 22:13:37 -0400

    Ah.

--- js/README.md
+++ js/README.md
@@ -2,21 +2,28 @@
 
 This directory contains the Firebase Firestore and Firebase Cloud Functions code handling data.
 
-## Installation
-
-First, configure the environment:
+## installation
 
 ```bash
 $ npm install --dev
 $ npm install --global firebase-cli
 $ firebase init
-$ export FIRESTORE_EMULATOR_HOST="localhost:8080"
 ```
 
-To start the emulators (need Firestore and Functions):
+## testing
+
+Testing the function locally requires:
+
+* Firebase Functions and Firestore emulators up and running.
+* A PostGIS database up and running and listening on port 5432.
+
+To start the emulators:
 
 ```bash
+$ export FIRESTORE_EMULATOR_HOST="localhost:8080"
 $ firebase emulators:start
 ```
 
-For now you can test everything is working by running `node test.js`.
+For instructions on starting the test PostGIS database refer to the `README` in the `python` folder.
+
+Assuming these conditions are satisfied, you can run the tests locally by executing `npm run-script test`.
--- js/test.js
+++ js/test.js
@@ -1,38 +1,38 @@
 const admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.firestore();
-const uuid4 = require('uuid').v4;
 
-async function insertRubbishRun(runID, pickupIDs) {
-    await db.collection('RubbishRunStory').doc(runID).set({
+// const uuid4 = require('uuid').v4;
+
+// async function insertRubbishRun(runID, pickupIDs) {
+//     await db.collection('RubbishRunStory').doc(runID).set({
+//         startLat: 0,
+//         startLong: 0,
+//         endLat: 0,
+//         endLat: 1,
+//         rubbishRunStoryModelID: runID,
+//         photoStoryIDs: pickupIDs
+//     });
+//     return;
+// }
+
+// async function insertPickup(lat, long) {
+//     const pickupID = uuid4();
+//     await db.collection('Story').doc(pickupID).set({
+//         photoStoryID: pickupID,
+//         lat: lat,
+//         long: long
+//     });
+//     return pickupID;
+// }
+
+(async () => {
+    const res = await db.collection('RubbishRunStory').doc(runID).get({
         startLat: 0,
         startLong: 0,
-        endLat: 0.01,
-        endLat: 0.01,
+        endLat: 0,
+        endLong: 1,
         rubbishRunStoryModelID: runID,
         photoStoryIDs: pickupIDs
     });
-    return;
-}
-
-async function insertPickup(runID) {
-    const pickupID = uuid4();
-    await db.collection('Story').doc(pickupID).set({
-        photoStoryID: pickupID,
-        lat: 0,
-        long: 0
-    });
-    return pickupID;
-}
-
-(async () => {
-    const runID = uuid4();
-    const pickupIDs = [];
-
-    for (let _ of [...Array(5).keys()]) {
-        const pickupID = await insertPickup(runID);
-        pickupIDs.push(pickupID);
-    }
-    
-    insertRubbishRun(runID, pickupIDs);
 })();
