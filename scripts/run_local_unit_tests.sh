c24e2798e5c21e88d77fa51ff71b04847579ea58
Aleksey Bilogur <aleksey@residentmar.io>
2020-12-27 23:59:59 -0500

    Tweak.

--- scripts/run_local_unit_tests.sh
+++ scripts/run_local_unit_tests.sh
@@ -6,5 +6,6 @@ set -e
 
 echo "Running Python unit tests..."
 pushd ../ 1>&0 && export RUBBISH_BASE_DIR=$(echo $PWD) && popd 1>&0
+pytest $RUBBISH_BASE_DIR/python/rubbish_geo_common/tests/tests.py
 pytest $RUBBISH_BASE_DIR/python/rubbish_geo_client/tests/tests.py
 pytest $RUBBISH_BASE_DIR/python/rubbish_geo_admin/tests/tests.py
